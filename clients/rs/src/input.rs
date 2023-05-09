54e3ee97b70fc8ee0ab0202d2d0cfb5fbafad657
namesty <nestor09amesty@gmail.com>
2023-05-10 00:20:01 +0200

    (chore): serde deserialization of Root dirs

--- clients/rs/src/input.rs
+++ clients/rs/src/input.rs
@@ -1,9 +1,46 @@
-use serde_json::Value;
+use serde::{de::{Deserializer, self}, Deserialize, Serialize};
 
-pub struct Input;
+#[derive(Serialize, Debug)]
+pub struct InputDir(String);
 
-impl Input {
-    pub fn expect_string(input: &Value) -> Result<String, String> {
-        Ok(input.as_str().expect("expected a string").to_string())
+#[derive(Deserialize)]
+struct InputDirIntermediate {
+    input: String,
+    #[serde(rename = "rootDir")]
+    root_dir: String,
+}
+
+impl<'de> Deserialize<'de> for InputDir {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let intermediate = InputDirIntermediate::deserialize(deserializer)?;
+
+        if intermediate.input.starts_with("$ROOT/") {
+          Ok(InputDir(intermediate.input.replace("$ROOT/", &intermediate.root_dir)))
+        } else {
+          Err(de::Error::custom("expected a string that starts with $ROOT/"))
+        }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::{json, Value};
+
+    use crate::input::InputDir;
+
+  #[test]
+  pub fn it_deserializes_root_dir() {
+    let value: Value = json!({
+      "input": "$ROOT/foo/bar",
+      "rootDir": "hee/"
+    });
+
+
+    let obj: InputDir = serde_json::from_value(value).unwrap();
+
+    assert_eq!(obj.0, "hee/foo/bar")
+  }
 }
