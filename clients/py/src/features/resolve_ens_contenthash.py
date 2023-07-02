e07fd3d7ab42e54ac9dff2d7672cd8e52baee6fd
Niraj Kamdar <nirajkamdar27@pm.me>
2023-07-02 16:10:33 +0700

    fix: all issues

--- clients/py/src/features/resolve_ens_contenthash.py
+++ clients/py/src/features/resolve_ens_contenthash.py
@@ -1,5 +1,33 @@
 from typing import Any
+from polywrap_client import PolywrapClient
+from polywrap_client_config_builder import PolywrapClientConfigBuilder
+from polywrap_core import UriPackage
+from polywrap_web3_config_bundle import get_web3_bundle
+
+from validators import validate_uri
+
 
 
 def run_test_case(input: Any) -> None:
-    pass
+    uri = validate_uri(input)
+
+    print(f"URI Authority: {uri.authority}")
+
+    config = (
+        PolywrapClientConfigBuilder()
+        .add(get_web3_bundle())
+        .build()
+    )
+
+    client = PolywrapClient(config)
+
+    print(f"Resolving: {uri}")
+
+    result = client.try_resolve_uri(uri)
+
+    match result:
+        case UriPackage():
+            print("Received: package")
+            print("Success!")
+        case _:
+            raise ValueError("Expected UriPackage")
--- clients/py/src/features/resolve_http.py
+++ clients/py/src/features/resolve_http.py
@@ -1,5 +1,32 @@
 from typing import Any
+from polywrap_client import PolywrapClient
+from polywrap_client_config_builder import PolywrapClientConfigBuilder
+from polywrap_core import UriPackage
+from polywrap_sys_config_bundle import get_sys_config
+
+from validators import validate_uri
 
 
 def run_test_case(input: Any) -> None:
-    pass
+    uri = validate_uri(input)
+
+    print(f"URI Authority: {uri.authority}")
+
+    config = (
+        PolywrapClientConfigBuilder()
+        .add(get_sys_config())
+        .build()
+    )
+
+    client = PolywrapClient(config)
+
+    print(f"Resolving: {uri}")
+
+    result = client.try_resolve_uri(uri)
+
+    match result:
+        case UriPackage():
+            print("Received: package")
+            print("Success!")
+        case _:
+            raise ValueError("Expected UriPackage")
