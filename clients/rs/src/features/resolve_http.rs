5fdd85ea98cca22f0eed6cbe871e73722ab6432d
namesty <nestor09amesty@gmail.com>
2023-07-10 12:04:55 +0200

    resolve http test fixed

--- clients/rs/src/features/resolve_http.rs
+++ clients/rs/src/features/resolve_http.rs
@@ -1,18 +1,18 @@
 use std::{error::Error};
 use polywrap_client::{client::PolywrapClient, builder::PolywrapClientConfig, core::{uri_resolver_handler::UriResolverHandler, resolution::uri_resolution_context::UriPackageOrWrapper, uri::Uri}};
+use polywrap_client_default_config::SystemClientConfig;
 use serde_json::Value;
 
 pub fn run_test_case(input: &Value) -> Result<(), Box<dyn Error>> {
   let uri: Uri = serde_json::from_value::<String>(input.clone())?.try_into()?;
   let uri_authority = uri.authority();
-  let uri_string = uri.to_string();
 
   println!("URI Authority: {uri_authority}");
 
-  let config = PolywrapClientConfig::new();
+  let config = SystemClientConfig::default();
   let client: PolywrapClient = PolywrapClient::new(config.into());
 
-  println!("Resolving: {uri_string}");
+  println!("Resolving: {}", input.to_string());
 
   let result = client.try_resolve_uri(&uri.try_into()?, None)?;
   let result_type = match result {
@@ -21,8 +21,8 @@ pub fn run_test_case(input: &Value) -> Result<(), Box<dyn Error>> {
     UriPackageOrWrapper::Package(_, _) => "package",
   };
 
-  if let UriPackageOrWrapper::Uri(_) = result {
-    println!("Received: '{result_type}'");
+  if let UriPackageOrWrapper::Package(_, _) = result {
+    println!("Received: {result_type}");
     println!("Success!");
   }
 
