49cb8e2ac2c3f2933893274fe02217ddcdb73f35
Cesar <cesarbrazon10@gmail.com>
2023-07-11 13:24:36 +0200

    chore: remove swift from install script & improve swift ci

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@ cd wraps && ./install.sh && cd ../
 cd verify && ./install.sh && cd ../
 cd clients/js && ./install.sh && cd ../../
 cd clients/kotlin && ./install.sh && cd ../../
-cd clients/swift && ./install.sh && cd ../../
