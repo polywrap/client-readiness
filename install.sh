f6889362c62b1d17679ba7210686ade09627b81a
Cesar <cesarbrazon10@gmail.com>
2023-07-11 12:15:55 +0200

    chore: add swift client to install script

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ cd wraps && ./install.sh && cd ../
 cd verify && ./install.sh && cd ../
 cd clients/js && ./install.sh && cd ../../
 cd clients/kotlin && ./install.sh && cd ../../
+cd clients/swift && ./install.sh && cd ../../
