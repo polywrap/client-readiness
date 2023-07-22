04e52cf8a77655138466c8201cacb620e4f8a318
Niraj Kamdar <nirajkamdar27@pm.me>
2023-07-22 16:04:22 +0700

    fix: issues

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -2,19 +2,48 @@ import { loadFeatureSpecs } from "./load-feature-specs";
 
 import path from "path";
 
+function parseTestCase(filterSpec: string) {
+  const filterSpecElem = filterSpec.split(".");
+  if (filterSpecElem.length < 2) {
+    return [filterSpecElem[0], new Set()];
+  }
+  return [filterSpecElem[0], new Set(filterSpecElem.slice(1))];
+}
+
+function parseFilterSpecs(filterSpecs: string) {
+  return Object.fromEntries(
+    filterSpecs
+      .split(",")
+      .filter((x) => x.trim())
+      .map(parseTestCase)
+  );
+}
+
+function setIntersection<T>(setA: Set<T>, setB: Set<T>): Set<T> {
+  const intersection = new Set<T>();
+  for (const elem of setB) {
+    if (setA.has(elem)) {
+      intersection.add(elem);
+    }
+  }
+  return intersection;
+}
+
+
 async function main() {
   // Optional 2nd argument, spec filter
-  let filter = process.argv.length > 2 ? process.argv[2] : undefined;
+  let filter_specs = process.argv.length > 2 ? process.argv[2] : "";
+  let filter_specs_map = parseFilterSpecs(filter_specs);
+  let filter_specs_set = new Set(Object.keys(filter_specs_map));
 
   const specs = loadFeatureSpecs(
     path.join(__dirname, "../../../specs")
   );
 
-  for (const specName of Object.keys(specs)) {
-    if (filter && filter !== specName) {
-      continue;
-    }
+  let specs_set = new Set(Object.keys(specs));
+  let specs_to_run = filter_specs_set.size ? setIntersection(specs_set, filter_specs_set) : specs_set;
 
+  for (const specName of specs_to_run) {
     const spec = specs[specName];
     const feature = await import(`./features/${specName}`);
 
@@ -30,8 +59,11 @@ async function main() {
     console.log("====================================");
 
     const testCases = specs[specName].cases;
+    const testCasesSet = new Set(Object.keys(testCases));
+    const filterTestCasesSet = filter_specs_map[specName] || new Set();
+    const testCasesToRun = filterTestCasesSet.size ? setIntersection(testCasesSet, filterTestCasesSet) : testCasesSet;
 
-    for (const testCase of Object.keys(testCases)) {
+    for (const testCase of testCasesToRun) {
       console.log(`$Test Start [${specName}.${testCase}]`);
 
       try {
--- clients/py/src/main.py
+++ clients/py/src/main.py
@@ -4,21 +4,31 @@ import os
 import sys
 import importlib.util
 
+def parse_test_case(filter_spec):
+    filter_spec_elem = filter_spec.split(".")
+    if len(filter_spec_elem) < 2:
+        return filter_spec_elem[0], set()
+    return filter_spec_elem[0], set(filter_spec_elem[1:])
+
+def parse_filter_specs(filter_specs):
+    return dict(map(parse_test_case, filter(lambda x: x.strip(), filter_specs.split(","))))
+
 
 def main():
     # Optional 2nd argument, spec filter_spec
-    filter_spec = sys.argv[1] if len(sys.argv) > 1 else None
+    filter_specs = sys.argv[1] if len(sys.argv) > 1 else None
+    filter_specs_map = parse_filter_specs(filter_specs)
+    filter_specs_set = set(filter_specs_map.keys())
 
     specs = load_feature_specs(
         os.path.join(os.path.dirname(__file__), "../../../specs")
     )
 
-    for spec_name, spec in specs.items():
-        if filter_spec:
-            filter_spec_elem = filter_spec.split(".")
-            if filter_spec_elem and filter_spec_elem[0] != spec_name:
-                continue
+    specs_set = set(specs.keys())
+    specs_to_run = specs_set.intersection(filter_specs_set) or specs_set
 
+    for spec_name in specs_to_run:
+        spec = specs[spec_name]
         feature_path = os.path.join(
             os.path.dirname(__file__), f"./features/{spec_name}.py"
         )
@@ -43,11 +53,14 @@ def main():
 
         test_cases = spec.cases
 
-        for test_case_name, test_case in test_cases.items():
-            print(f"$Test Start [{spec_name}.{test_case_name}]")
+        test_cases_set = set(test_cases.keys())
+        filter_test_cases_set = filter_specs_map.get(spec_name, set())
 
-            if filter_spec and len(filter_spec_elem) > 1 and filter_spec_elem[1] != test_case_name:
-                continue
+        test_cases_to_run = test_cases_set.intersection(filter_test_cases_set) or test_cases_set
+
+        for test_case_name in test_cases_to_run:
+            test_case = test_cases[test_case_name]
+            print(f"$Test Start [{spec_name}.{test_case_name}]")
 
             try:
                 feature.run_test_case(test_case.input)
